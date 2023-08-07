47c1a37203b2b0d1f1e1ff3705f25c751fab2656
Ian Zelbo <113198991+izelbo129@users.noreply.github.com>
2023-08-07 18:28:53 -0400

    Add files via upload

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ian Zelbo on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
